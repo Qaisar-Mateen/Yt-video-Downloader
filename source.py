3cb8b5471e83463a9989fe03321494ec630bbf94
Qaisar-Mateen <qaisarmateen313@gmail.com>
2024-01-16 21:34:12 +0500

    r1

--- source.py
+++ source.py
@@ -1,13 +1,16 @@
 from pytube import YouTube
 import tkinter as tk
+from tkinter import *
 from tkinter import filedialog
+
+res_stream = None
 
 def download_video(url, save_path):
     try:
         yt = YouTube(url)
         streams = yt.streams.filter(progressive=True, file_extension="mp4")
-        highest_res_stream = streams.get_highest_resolution()
-        highest_res_stream.download(output_path=save_path)
+        res_stream = streams.get_highest_resolution()
+        res_stream.download(output_path=save_path)
         print("Video downloaded successfully!")
     except Exception as e:
         print(e)
@@ -21,7 +24,7 @@ def open_file_dialog():
 
 if __name__ == "__main__":
     root = tk.Tk()
-    root.withdraw()
+    
 
     video_url = input("Please enter a YouTube url: ")
     save_dir = open_file_dialog()
