d75837c5059c821dc7d357c105edff76dc785edb
Qaisar-Mateen <qaisarmateen313@gmail.com>
2024-01-17 19:38:46 +0500

    r5

--- source.py
+++ source.py
@@ -1,3 +1,4 @@
+from turtle import bgcolor
 from pytube import YouTube
 import tkinter as tk
 from tkinter import SUNKEN, filedialog
@@ -32,10 +33,10 @@ if __name__ == "__main__":
     root.geometry("500x350")
     root.title("YouTube Video Downloader")
 
-    frame = tk.Frame()
+    frame = tk.Frame(root,padx=20, pady=20, bg="red").pack()
 
     # thumbnail frame
-    pic_frame = tk.Frame(root , width=250, height=141, borderwidth=2, relief="raised", bg="black")
+    pic_frame = tk.Frame(frame , width=250, height=141, borderwidth=2, relief="raised", bg="black")
     pic_frame.pack(anchor="center", padx=10, pady=10)
     img = Image.open("no image.png")
     img.thumbnail((250, 141), Image.LANCZOS)
