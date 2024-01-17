2620f6e6fa8f21da9489f35485662140fefd8aef
Qaisar-Mateen <qaisarmateen313@gmail.com>
2024-01-17 23:58:15 +0500

    r7

--- source.py
+++ source.py
@@ -48,7 +48,7 @@ if __name__ == "__main__":
     thumbnail_label = tk.Label(pic_frame, image=thumbnail).pack()
 
     input_frame = tk.Frame(frame, bg="blue").pack(anchor="center", fill=tk.X + tk.Y ,padx=10, pady=10)
-
+    urlbox = tk.Entry(input_frame, width=50, borderwidth=2, relief="sunken", text="Enter Youtube Video URL").pack(anchor="center", padx=10, pady=10)
     root.mainloop()
 
     # video_url = input("Please enter a YouTube url: ")
