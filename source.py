a7200be0df066221375a222cdde93b2a2485f2a9
Qaisar-Mateen <qaisarmateen313@gmail.com>
2024-01-20 20:28:25 +0500

    sad

--- source.py
+++ source.py
@@ -36,25 +36,30 @@ if __name__ == "__main__":
     root.title("YouTube Video Downloader")
 
     root.grid_columnconfigure(0, weight=1)
+    root.grid_rowconfigure(1, weight=1)
     # frame = ctk.CTkFrame(root)
     # frame.grid(row=0, column=0, sticky="nsew", padx=10, pady=10)
-
-    but = ctk.CTkButton(root, text="Download")
-    but.grid(row=1, column=0, sticky="ew", padx=20, pady=(0,20))
 
     # thumbnail frame
     pic_frame = ctk.CTkFrame(root, width=260, height=151)
     pic_frame.grid(row=0, column=0, padx=10, pady=(20, 10))
 
+    # thumbnail image
     thumbnail_image = ctk.CTkImage(Image.open("no image.png"), size=(250, 141))
     thumbnail_image_label = ctk.CTkLabel(pic_frame, image=thumbnail_image)
     thumbnail_image_label.grid(row=0, column=0, padx=25, pady=25)
 
+    # input frame
+    input_frame = ctk.CTkFrame(root)
+    input_frame.grid(row=1, column=0, sticky="nsew", padx=30, pady=(5,30))
 
     # input_frame = tk.Frame(frame)
     # input_frame.pack(anchor="center", fill=tk.X ,padx=10, pady=10)
     # urlbox = tk.Entry(input_frame, width=50, borderwidth=2, relief="groove")
     # urlbox.pack(anchor="center", padx=10, pady=10)
+
+    # but = ctk.CTkButton(input_frame, text="Download")
+    # but.grid(row=0, column=0, sticky="s", padx=40, pady=40)
 
     root.mainloop()
 
