75b056aa542a2b315bbd9ad366570d71d50b599e
Qaisar-Mateen <qaisarmateen313@gmail.com>
2024-01-24 23:36:28 +0500

    added the commented previous code for image display and resize

--- source.py
+++ source.py
@@ -89,6 +89,10 @@ def update_window(title, author, publish_date, thumbnail_url, avail_resolutions)
     button_color = "#E20000"
     button_color_hov = "red"
 
+    # thumbnail_image = ctk.CTkImage(Image.open(requests.get(thumbnail_url, stream=True).raw), size=(280, 157))
+    # thumbnail_image_label.configure(image=thumbnail_image)
+    # thumbnail_image_label.update()
+    
     img = Image.open(requests.get(thumbnail_url, stream=True).raw)
     img.thumbnail((280, 157), Image.LANCZOS)
     thumbnail_image = ctk.CTkImage(img)
